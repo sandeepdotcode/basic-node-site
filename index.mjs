b88bf4b89bcc95c8aba0129f2becf79e53226f85
Sandeep K K <sandeepdotcode@gmail.com>
2023-08-29 22:46:09 +0530

    Modify server code to follow MDN guide

--- index.mjs
+++ index.mjs
@@ -1,52 +1,46 @@
 import { createServer } from "node:http";
-import { join as joinPath, dirname, extname } from "node:path";
-import { fileURLToPath } from "node:url";
-import { readFile } from "node:fs/promises";
-import getContentType from "./lib/contentType.mjs";
+import { join as joinPath, extname } from "node:path";
+import { access as accessFile } from "node:fs/promises";
+import { createReadStream } from "node:fs";
+import MIME_TYPES from "./lib/contentType.mjs";
 
 const hostname = "127.0.0.1";
 const port = process.env.PORT ?? 3000;
+const STATIC_PATH = joinPath(process.cwd(), './public');
 
-const server = createServer((req, res) => {
+const toBool = [() => true, () => false];
+
+async function prepareFile(url) {
+  const paths = [STATIC_PATH, url];
+  const htmlPaths = [STATIC_PATH, 'pages', url + '.html'];
+  if (url.endsWith("/")) {
+    paths.push("index.html");
+  }
+
+  const filePath = (!url.endsWith("/") && extname(url) === '') ? joinPath(...htmlPaths) : joinPath(...paths);
+  const pathTraversal = !filePath.startsWith(STATIC_PATH);   // security check to prevet path traversal vulnerabilites
+  const fileExists = await accessFile(filePath).then(...toBool);
+  const fileFound = !pathTraversal && fileExists;
+
+  const streamPath = fileFound ? filePath : joinPath(STATIC_PATH, './pages/404.html')
+  const ext = extname(streamPath).substring(1).toLowerCase();
+  const stream = createReadStream(streamPath);
+
+  return { found: fileFound, ext, stream };
+}
+
+const server = createServer(async (req, res) => {
   // res.statuscode = 200;
   // res.setHeader('Content-Type', 'text/plain');
   // res.end('Hello World!\n');
 
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-  const fileExt = extname(req.url);
+  const file = await prepareFile(req.url);
+  const statuscode = file.found ? 200 : 400;
+  const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
 
-
-  if (fileExt === '') {
-    const filePath = joinPath(
-      __dirname, 'public'
-      , req.url === '/' ? 'index.html' : joinPath('pages', req.url + '.html')
-    );
-
-    readFile(filePath)
-    .then(function(content) {
-      res.writeHead(200, { 'Content-Types': 'text/html' });
-      res.end(content, 'utf-8');
-    })
-    .catch(function(error) {
-      if (error.code === 'ENOENT') {
-        readFile(joinPath(__dirname, 'public', 'pages', '404.html'))
-        .then(function(content) {
-          res.writeHead(404, { 'Content-Type': 'text/html' });
-          res.end(content, 'utf-8');
-        })
-        .catch(function(error) {
-          res.writeHead(500);
-          res.end(`Server Error: ${error.code}`);
-        });
-      } else {
-        res.writeHead(500);
-        res.end(`Server Error: ${error.code}`);
-      }
-    });
-  } else {
-    let contentType = getContentType(fileExt);
-  }
-
+  res.writeHead(statuscode, { "Content-Type": mimeType });
+  file.stream.pipe(res);
+  console.log(`${req.method} ${req.url} ${statuscode}`)
 });
 
 server.listen(port, hostname, () => {
